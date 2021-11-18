11c4b616c7695cccb7a231a2da9b547f6c14cd1c
Cather-Zhang <cather0215@gmail.com>
2021-11-18 17:41:59 -0500

    a

--- src/main/java/edu/wpi/cs/sophex/demo/CalculatorHandler.java
+++ src/main/java/edu/wpi/cs/sophex/demo/CalculatorHandler.java
@@ -33,6 +33,7 @@ public class CalculatorHandler implements RequestHandler<AddRequest,AddResponse>
 	public static final String TEST_BUCKET = "testconstants";
 
 	//new changes
+	// second change
 	@Override
 	public AddResponse handleRequest(AddRequest req, Context context) {
 		logger = context.getLogger();
