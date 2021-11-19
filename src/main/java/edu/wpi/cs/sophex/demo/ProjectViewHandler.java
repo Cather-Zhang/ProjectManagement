a1dfffe84dee1cbede32ecd7fc31c1ee64b307cc
brsahagian <44908953+brsahagian@users.noreply.github.com>
2021-11-19 15:02:02 -0500

    ProjectViewResponseCaseDraft

--- src/main/java/edu/wpi/cs/sophex/demo/ProjectViewHandler.java
+++ src/main/java/edu/wpi/cs/sophex/demo/ProjectViewHandler.java
@@ -1,5 +1,37 @@
 package edu.wpi.cs.sophex.demo;
 
-public class ProjectViewHandler {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
+import edu.wpi.cs.heineman.demo.db.ProjectsDAO;
+import edu.wpi.cs.sophex.demo.http.CreateProjectRequest;
+import edu.wpi.cs.sophex.demo.http.CreateProjectResponse;
+import edu.wpi.cs.sophex.demo.model.Project;
+
+public class ProjectViewHandler {
+LambdaLogger logger;
+	
+
+	public CreateProjectResponse handleResponse(String projectName) throws Exception {
+		boolean fail = false;
+		String failMessage = "";
+		if(loadProjectUserFromRDS(projectName) == null) {
+			failMessage = projectName + " does not exist.";
+			fail = true;
+		}
+
+		CreateProjectResponse response;
+		if (fail) {
+			response = new CreateProjectResponse(400, failMessage);
+		} else {
+			response = new CreateProjectResponse(projectName, 200);  // success
+		}
+
+		return response; 
+	}
+	public Project loadProjectUserFromRDS(String projectName) throws Exception {
+		ProjectsDAO dao = new ProjectsDAO();
+		Project p = dao.getProjectUser(projectName);
+		return p;
+	}
 }
--- src/main/java/edu/wpi/cs/sophex/demo/http/ProjectViewResponse.java
+++ src/main/java/edu/wpi/cs/sophex/demo/http/ProjectViewResponse.java
@@ -1,5 +1,46 @@
 package edu.wpi.cs.sophex.demo.http;
 
+import java.util.List;
+
+import edu.wpi.cs.heineman.demo.db.ProjectsDAO;
+import edu.wpi.cs.sophex.demo.model.Project;
+import edu.wpi.cs.sophex.demo.model.Task;
+import edu.wpi.cs.sophex.demo.model.Teammate;
+
 public class ProjectViewResponse {
-	//class for project view response
+		public List<Task> tasks;
+		public List<Teammate> teammates;
+		public int statusCode;
+		public String error;
+		
+		public ProjectViewResponse (String projectName, int statusCode) throws Exception {
+			Project p = loadProjectUserFromRDS(projectName); //need to find project with this name
+			teammates = p.getTeammates();
+			tasks = p.getTasks();
+			this.statusCode = statusCode;
+			this.error = "";
+		}
+		
+		public ProjectViewResponse (int statusCode, String errorMessage) {
+			this.teammates = null;
+			this.tasks = null;
+			this.statusCode = statusCode;
+			this.error = errorMessage;
+		}
+		
+		public String toString() {
+			if (statusCode / 100 == 2) {  // too cute?
+				return "";
+			} else {
+				return "ErrorResult(" + statusCode + ", err=" + error + ")";
+			}
+		}
+		
+		public Project loadProjectUserFromRDS(String projectName) throws Exception {
+			ProjectsDAO dao = new ProjectsDAO();
+			Project p = dao.getProjectUser(projectName);
+			return p;
+		}
+	
+
 }
