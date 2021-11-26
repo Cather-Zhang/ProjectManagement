92eac46aeaebbfa1be1986d95da89556517632fc
brsahagian <44908953+brsahagian@users.noreply.github.com>
2021-11-26 12:36:26 -0500

    Delete Project Handlers

--- src/main/java/sophex/handler/admin/DeleteProjectHandler.java
+++ src/main/java/sophex/handler/admin/DeleteProjectHandler.java
@@ -3,15 +3,37 @@ package sophex.handler.admin;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import sophex.db.ProjectsDAO;
 import sophex.http.admin.DeleteProjectRequest;
 import sophex.http.admin.DeleteProjectResponse;
+import sophex.model.Project;
 
 public class DeleteProjectHandler implements RequestHandler<DeleteProjectRequest, DeleteProjectResponse>{
 
 	@Override
-	public DeleteProjectResponse handleRequest(DeleteProjectRequest input, Context context) {
-		// TODO Auto-generated method stub
-		return null;
+	public DeleteProjectResponse handleRequest(DeleteProjectRequest req, Context context) {
+		boolean fail = false;
+		String failMessage = "";
+		DeleteProjectResponse response;
+		try {
+			if(loadProjectUserFromRDS(req.getProjectName()) == null) {
+				failMessage = req.getProjectName() + " does not exist.";
+				fail = true;
+			}		
+			if (fail) {
+				response = new DeleteProjectResponse(failMessage,400); //fail
+			} else {
+					response = new DeleteProjectResponse(loadProjectUserFromRDS(req.getProjectName()));  // success
+				}
+			} catch (Exception e) {
+				response = new DeleteProjectResponse("Unable to delete project: " + req.getProjectName() + "(" + e.getMessage() + ")",400);
+			}
+			return response; 
+		}
+			
+		public Project loadProjectUserFromRDS(String projectName) throws Exception {
+			ProjectsDAO dao = new ProjectsDAO();
+			Project p = dao.getProjectUser(projectName);
+			return p;
+		}
 	}
-
-}
--- src/main/java/sophex/http/admin/DeleteProjectResponse.java
+++ src/main/java/sophex/http/admin/DeleteProjectResponse.java
@@ -1,5 +1,41 @@
 package sophex.http.admin;
 
-public class DeleteProjectResponse {
+import sophex.model.Project;
 
+public class DeleteProjectResponse {
+	public Project project;
+	public int statusCode;
+	public String error;
+		
+	/**
+	 * success, status = 200
+	 * @param project
+	 */
+	public DeleteProjectResponse (Project project) {
+		this.project = project; 
+		this.statusCode = 200;
+		this.error = "";
+	}
+	
+	/**
+	 * fail
+	 * @param statusCode
+	 * @param errorMessage
+	 */
+	public DeleteProjectResponse (String errorMessage, int statusCode) {
+		this.project = null; // doesn't matter since error
+		this.statusCode = statusCode;
+		this.error = errorMessage;
+	}
+	
+	public String toString() {
+		if (statusCode / 100 == 2) { 
+			return "Project(" + project + ")";
+		} else if (statusCode == 400) {   //SOME ERROR CODE
+			return ("Project " + project.getname() + " does not exist");
+		}
+		{
+			return "ErrorResult(" + statusCode + ", err=" + error + ")";
+		}
+	}
 }
