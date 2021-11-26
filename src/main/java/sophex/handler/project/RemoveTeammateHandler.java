d5cf8c2d5e24564bd0d73723da7cb8f7d48b7e0d
brsahagian <44908953+brsahagian@users.noreply.github.com>
2021-11-26 13:16:36 -0500

    Remove Teammate Handlers

--- src/main/java/sophex/handler/project/RemoveTeammateHandler.java
+++ src/main/java/sophex/handler/project/RemoveTeammateHandler.java
@@ -3,16 +3,39 @@ package sophex.handler.project;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import sophex.db.ProjectsDAO;
 import sophex.http.project.RemoveTeammateRequest;
 import sophex.http.project.RemoveTeammateResponse;
+import sophex.model.Project;
 
 public class RemoveTeammateHandler implements RequestHandler<RemoveTeammateRequest, RemoveTeammateResponse>{
 
 	@Override
 	public RemoveTeammateResponse handleRequest(RemoveTeammateRequest input, Context context) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+				boolean fail = false;
+				String failMessage = "";
+				RemoveTeammateResponse response;
+				try {
+					if(loadProjectUserFromRDS(input.getProjectName()) == null) {
+						failMessage = input.getProjectName() + " does not exist.";
+						fail = true;
+					}		
+					if (fail) {
+						response = new RemoveTeammateResponse(failMessage,400); //fail
+					} else {
+							response = new RemoveTeammateResponse(loadProjectUserFromRDS(input.getProjectName()), req.getTeammateName());  // success
+						}
+					} catch (Exception e) {
+						response = new RemoveTeammateResponse("Unable to remove teammate: " + input.getProjectName() + "(" + e.getMessage() + ")",400);
+					}
+					return response; 
+				}
+					
+				public Project loadProjectUserFromRDS(String projectName) throws Exception {
+					ProjectsDAO dao = new ProjectsDAO();
+					Project p = dao.getProjectUser(projectName);
+					return p;
+				}
+			}
 
 
-}
--- src/main/java/sophex/http/project/RemoveTeammateRequest.java
+++ src/main/java/sophex/http/project/RemoveTeammateRequest.java
@@ -1,5 +1,23 @@
 package sophex.http.project;
 
 public class RemoveTeammateRequest {
+	public String teammateName;
+	public String projectName;
 
+	public String getTeammateName() { return teammateName; }
+	public void setTeammateName(String teammateName) { this.teammateName = teammateName; }
+	public String getProjectName() {return projectName;}
+	public void setProjectName(String projectName) {this.projectName = projectName;}
+	
+
+	public String toString() {
+		return "Add (" + teammateName + ") to (" + projectName + ")";
+	}
+	
+	public RemoveTeammateRequest (String teammateName, String projectName) {
+		this.teammateName = teammateName;
+		this.projectName = projectName;
+	}
+	
+	public RemoveTeammateRequest() {}
 }
